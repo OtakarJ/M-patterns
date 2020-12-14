29cc63d7d436c09a066be54cbc08b857c6be550e
Otakar <71336432+OtakarJ@users.noreply.github.com>
2020-12-14 17:13:58 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -7,11 +7,19 @@ inputs.forEach(function(i){
         var variable2 = document.querySelector('input[name="pop"]:checked').value;
         var variable3 = document.querySelector('input[name="pattern"]:checked').value;
    
-        var customClass = "bg--" + variable1 + "-pop-" + variable2 + "-" + variable3;
+        if (variable1 != variable2) {
+            var backgroundClass = "bg--" + variable1 + "-pop-" + variable2 + "-" + variable3;
+            document.getElementById('pop').style.display = 'inline-block';
+        }
+        else {
+            var backgroundClass = "bg--" + variable1 + "-" + variable3;
+            document.getElementById('pop').style.display = 'none';
+
+    }
 
         document.getElementById('pattern-demo').className = "";
-        document.getElementById('pattern-demo').classList.add(customClass);
-        //document.getElementById('class').innerText = customClass;
+        document.getElementById('pattern-demo').classList.add(backgroundClass);
+        //document.getElementById('class').innerText = backgroundClass;
         document.getElementById('value-color').innerText = variable1;
         document.getElementById('value-pop').innerText = variable2;
         document.getElementById('value-type').innerText = variable3;
